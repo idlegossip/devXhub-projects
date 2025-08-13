ba674bb0594d68f864438eeddc6be370c50325ee
Nazim Uddin <nazimuddin223t@gmail.com>
2025-08-13 12:31:49 +0600

    initial code.

---
+++ devxhub-Intern/src/main/java/com/project/devxhub_Intern/DevxhubInternApplication.java
@@ -0,0 +1,13 @@
+package com.project.devxhub_Intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevxhubInternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevxhubInternApplication.class, args);
+	}
+
+}
---
+++ devxhub-Intern/src/test/java/com/project/devxhub_Intern/DevxhubInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.devxhub_Intern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevxhubInternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
